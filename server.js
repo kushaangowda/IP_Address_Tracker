bf22182a50986d7117582d68e5269f3643a564d1
Kushaan Gowda <53338864+kushaangowda@users.noreply.github.com>
2020-09-30 13:26:07 +0530

    added ipify functionality

--- server.js
+++ server.js
@@ -2,6 +2,11 @@ const express = require('express');
 const logger = require('morgan');
 const ejs = require('ejs');
 const bodyParser = require('body-parser'); 
+const https = require('https');
+const api_key = 'at_CghMsaA7R7ZmCFYvbtkqe5TUi1wE1';
+const api_url = 'https://geo.ipify.org/api/v1?';
+
+
 
 var port = process.env.PORT || 3000;
 
@@ -27,3 +32,21 @@ app.listen(port, () => console.log(`listening to PORT ${port}...`));
 app.get('/',(req,res)=>{
 	res.render('index.ejs');
 })
+
+app.post('/',(req,res)=>{
+	var ip_address = req.body.ip_address;
+	var url = api_url + 'apiKey=' + api_key + '&ipAddress=' + ip_address;
+	getLocation(res,ip_address,url);
+})
+
+
+async function getLocation(res,ip,url){
+	https.get(url, function(response) {
+	    var str = '';
+	    response.on('data', function(chunk) { str += chunk; });
+	    response.on('end', function() { 
+	    	console.log(str); 
+			res.render('index.ejs',{str:str});
+	    });
+	}).end();
+}
